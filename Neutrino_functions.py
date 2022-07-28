fc0e0dea067ae9bcc06d88599e15f2ba41e61491
Alex Kedziora <Aleksander2116k@gmail.com>
2022-07-28 11:21:07 +0100

    Plotting function and Main file updated
    - Added some comments to MainAnalysis
    - Corrected neutrino_functions

--- Neutrino_functions.py
+++ Neutrino_functions.py
@@ -5,19 +5,32 @@ import numpy as np
 #log_scale=logscale, labels = labels
 
 def histogram_plot(MC_frame, variable, bins, name, scaling, plot_data = None, logscale = None, dataFrame = None):
+    """
+    MC_frame: pandas dataframe - MC dataframe
+    variable: string - name of the variable
+    bins: int - number of bins
+    name: string - name of the plot. The plot is saved
+    scaling: array/list - weights you want to apply on MC data
+    plot_data: boolean - 
+    """
     #x = 0.5*(bins[1:]+ bins[:-1])
     
     if (plot_data == None):
         fig = plt.figure(figsize=(15,10))
+        
         labels=[r"$\nu$ NC", r"$\nu_{\mu}$ CC", r"$\nu_e$ CC", r"EXT", r"Out. fid. vol.", r"Cosmic"]
         sns.histplot(data=MC_frame, x= variable , hue="category", multiple="stack", palette = 'deep', weights = scaling, bins=bins, legend = False, log_scale=logscale)
-        plt.legend(title='Run 30', loc='upper right', labels=[r"$\nu$ NC", r"$\nu_{\mu}$ CC", r"$\nu_e$ CC", r"EXT", r"Out. fid. vol.", r"Cosmic"])
-        plt.savefig('name.jpg', dpi=300) 
+        
+        plt.legend(title='Run 3', loc='upper right', labels=[r"$\nu$ NC", r"$\nu_{\mu}$ CC", r"$\nu_e$ CC", r"EXT", r"Out. fid. vol.", r"Cosmic"])
+        plt.xlabel(variable)
+        plt.ylabel("Events")
+        plt.savefig(name+'.jpg', dpi=300) 
         plt.show()
         
 
     else:
         fig_data = plt.figure(figsize=(15,10))
+        
         Data_fig = sns.histplot(data=dataFrame, x=variable, bins=bins, legend = False, log_scale=logscale)
         bars = Data_fig.patches
         
@@ -25,38 +38,25 @@ def histogram_plot(MC_frame, variable, bins, name, scaling, plot_data = None, lo
         x = [patch.get_x() for patch in bars]
         w = [patch.get_width() for patch in bars]
         plt.close(fig_data)
-        #y_real = np.array(y_real)
-        fig = plt.figure(figsize=(15,10))
-        labels=[r"$\nu$ NC", r"$\nu_{\mu}$ CC", r"$\nu_e$ CC", r"EXT", r"Out. fid. vol.", r"Cosmic", r"Data"]
-        sns.histplot(data=MC_frame, x= variable , hue="category", multiple="stack", palette = 'deep', weights = scaling, bins = x, legend = False, log_scale=logscale)
         del x[-1]
         del heights[-1]
         new_bins = [start+w[0]/2 for start in x]
-        plt.errorbar(new_bins, heights , xerr=w[0]/2, fmt='.k');
-        plt.legend(title='Run 30', loc='upper right', labels=[r"$\nu$ NC", r"$\nu_{\mu}$ CC", r"$\nu_e$ CC", r"EXT", r"Out. fid. vol.", r"Cosmic"])
-        plt.xlabel("Reconstructed numu energy (Gev)")
+        #y_real = np.array(y_real)
+        
+        fig = plt.figure(figsize=(15,10))
+        sns.histplot(data=MC_frame, x= variable , hue="category", multiple="stack", palette = 'deep', weights = scaling, bins = x, legend = False, log_scale=logscale)
+        
+        plt.errorbar(new_bins, heights , xerr=w[0]/2, fmt='.k')
+        labels=[r"$\nu$ NC", r"$\nu_{\mu}$ CC", r"$\nu_e$ CC", r"EXT", r"Out. fid. vol.", r"Cosmic", r"Data"]
+        plt.legend(title='Run 3', loc='upper right', labels=labels)
+        
+        if variable == trk_energy_tot:
+            variable = "Reconstructed numu energy (Gev)"
+            
+        plt.xlabel(variable)
         plt.ylabel("Events")
-        plt.savefig('name.jpg', dpi=300) 
+        plt.savefig(name+'.jpg', dpi=300) 
         
         
     return 
-    
-    
-#Return element from index
-def element_index(vector,index,defval=9999.):
-    import awkward 
-    #print ('vector check....')
-    #print (idx)
-    #print (len(pidv))
-    return awkward.fromiter([pidv[tid] if ( (tid<len(pidv) ) & (tid>=0)) else defval for pidv,tid in zip(vector,index)])
-
-#Return index from element
-def index(argidx,vecsort,mask):
-    import awkward 
-    vid = vecsort[mask]
-    sizecheck = argidx if argidx>=0 else abs(argidx)-1
-    # find the position in the array after masking
-    mskd_pos = [v.argsort()[argidx] if len(v)>sizecheck else -1 for v in vid]
-    # go back to the corresponding position in the origin array before masking
-    result = [[i for i, n in enumerate(m) if n == 1][p] if (p)>=0 else -1 for m,p in zip(mask,mskd_pos)]
-    return result
+   
